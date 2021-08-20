1fe0b846ef5ef50aa7bee7b6e62a0e6c81bc179a
Ahmed Osama <marvelphp5@gmail.com>
2021-08-20 05:03:37 +0200

    Added more assertion for rules resolver to check against array of rule instances

--- tests/Unit/Validation/RulesResolverTest.php
+++ tests/Unit/Validation/RulesResolverTest.php
@@ -2,6 +2,8 @@
 
 use PHPUnit\Framework\TestCase;
 use SecTheater\Validation\RulesResolver;
+use SecTheater\Validation\Rules\EmailRule;
+use SecTheater\Validation\Rules\AlphaNumRule;
 use SecTheater\Validation\Rules\RequiredRule;
 use SecTheater\Validation\Rules\Contract\Rule;
 
@@ -36,5 +38,18 @@ class RulesResolverTest extends TestCase
         foreach ($instances as $instance) {
             $this->assertInstanceOf(Rule::class, $instance);
         }
+    }
+
+    public function test_it_accepts_array_of_rule_instances()
+    {
+        $rules = [
+            new RequiredRule(),
+            new EmailRule(),
+            new AlphaNumRule()
+        ];
+
+        $instances = RulesResolver::make($rules);
+
+        $this->assertContainsOnlyInstancesOf(Rule::class, $instances);
     }
 }
