13d55274bfade80a4c2424a580fbce226574bc1b
Ahmed Osama <marvelphp5@gmail.com>
2021-08-27 23:31:57 +0200

    Added test cases for finding the singular version of a word

--- tests/Unit/Support/StrTest.php
+++ tests/Unit/Support/StrTest.php
@@ -5,6 +5,32 @@ use PHPUnit\Framework\TestCase;
 
 class StrTest extends TestCase
 {
+    protected $irregulars = [
+        'move' => 'moves',
+        'foot' => 'feet',
+        'sex' => 'sexes',
+        'child' => 'children',
+        'man' => 'men',
+        'woman' => 'women',
+        'tooth' => 'teeth',
+        'person' => 'people',
+        'leaf' => 'leaves',
+        'knife' => 'knives',
+        'life' => 'lives'
+    ];
+
+    protected $uncountable = [
+        'sheep',
+        'fish',
+        'deer',
+        'series',
+        'species',
+        'money',
+        'rice',
+        'information',
+        'equipment'
+    ];
+
     public function test_it_pluralizes_a_word_for_regular_singulars()
     {
         $words = ['quiz', 'category', 'bus', 'user', 'cart'];
@@ -17,41 +43,46 @@ class StrTest extends TestCase
 
     public function test_it_returns_the_same_words_if_words_are_uncountable()
     {
-        $uncountable = [
-            'sheep',
-            'fish',
-            'deer',
-            'series',
-            'species',
-            'money',
-            'rice',
-            'information',
-            'equipment'
-        ];
-
-        foreach ($uncountable as $word) {
+        foreach ($this->uncountable as $word) {
             $this->assertEquals($word, Str::plural($word));
         }
     }
 
     public function test_it_finds_the_plural_of_irregular_singulars()
     {
-        $irregulars = [
-            'move' => 'moves',
-            'foot' => 'feet',
-            'sex' => 'sexes',
-            'child' => 'children',
-            'man' => 'men',
-            'woman' => 'women',
-            'tooth' => 'teeth',
-            'person' => 'people',
-            'leaf' => 'leaves',
-            'knife' => 'knives',
-            'life' => 'lives'
+        foreach ($this->irregulars as $singular => $plural) {
+            $this->assertEquals($plural, Str::plural($singular));
+        }
+    }
+
+    public function test_it_finds_the_singular_of_a_regular_word()
+    {
+        $regularPlurals = [
+            'users' => 'user',
+            'carts' => 'cart',
+            'cups' => 'cup',
+            'websites' => 'website',
+            'passwords' => 'password',
+            'usernames' => 'username',
+            'emails' => 'email'
         ];
 
-        foreach ($irregulars as $singular => $plural) {
-            $this->assertEquals($plural, Str::plural($singular));
+        foreach ($regularPlurals as $plural => $singular) {
+            $this->assertEquals($singular, Str::singular($plural));
+        }
+    }
+
+    public function test_it_returns_the_same_word_if_its_uncountable()
+    {
+        foreach ($this->uncountable as $word) {
+            $this->assertEquals($word, Str::singular($word));
+        }
+    }
+
+    public function test_it_finds_the_singular_for_irregular_plurals()
+    {
+        foreach (array_flip($this->irregulars) as $singular => $plural) {
+            $this->assertEquals($plural, Str::singular($singular));
         }
     }
 }
